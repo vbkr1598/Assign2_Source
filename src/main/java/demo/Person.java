67198b6ea13c7f08d9fb9773e2d66e00ef8ffde6
vbkr1598 <68968706+vbkr1598@users.noreply.github.com>
2021-01-08 18:48:15 +0530

    added servelet

--- src/main/java/demo/Person.java
+++ src/main/java/demo/Person.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@WebServlet(name="Person",urlPatterns={"/person"})
 @Entity
 public class Person {
 
